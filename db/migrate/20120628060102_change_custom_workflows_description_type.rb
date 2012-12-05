9476dea998ff1fe74a23d54429788075e77f388c
Anton Argirov <anton.argirov@gmail.com>
2012-12-05 15:41:50 +0700

    Не проходит migrate (#9403)

--- db/migrate/20120628060102_change_custom_workflows_description_type.rb
+++ db/migrate/20120628060102_change_custom_workflows_description_type.rb
@@ -1,9 +1,9 @@
 class ChangeCustomWorkflowsDescriptionType < ActiveRecord::Migration
   def self.up
-    change_column :custom_workflows, :description, :text, :null => false
+    change_column :custom_workflows, :description, :text, :null => false, :default => ""
   end
 
   def self.down
-    change_column :custom_workflows, :description, :string, :null => false
+    change_column :custom_workflows, :description, :string, :null => false, :default => ""
   end
 end
