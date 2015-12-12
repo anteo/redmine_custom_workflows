3f0c80232ae038361c44e327a7dbb859a33f1348
Anton Argirov <anton.argirov@gmail.com>
2015-12-12 20:29:22 +0600

    New observable objects added (TimeEntry)

---
+++ lib/redmine_custom_workflows/time_entry_patch.rb
@@ -0,0 +1,38 @@
+module RedmineCustomWorkflows
+  module TimeEntryPatch
+
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        before_save :before_save_custom_workflows
+        after_save :after_save_custom_workflows
+        before_destroy :before_destroy_custom_workflows
+        after_destroy :after_destroy_custom_workflows
+      end
+    end
+
+    module InstanceMethods
+      def before_save_custom_workflows
+        @time_entry = self
+        @saved_attributes = attributes.dup
+        CustomWorkflow.run_shared_code(self)
+        CustomWorkflow.run_custom_workflows(:time_entry, self, :before_save)
+        errors.empty? && (@saved_attributes == attributes || valid?)
+      ensure
+        @saved_attributes = nil
+      end
+
+      def after_save_custom_workflows
+        CustomWorkflow.run_custom_workflows(:time_entry, self, :after_save)
+      end
+
+      def before_destroy_custom_workflows
+        CustomWorkflow.run_custom_workflows(:time_entry, self, :before_destroy)
+      end
+
+      def after_destroy_custom_workflows
+        CustomWorkflow.run_custom_workflows(:time_entry, self, :after_destroy)
+      end
+    end
+  end
+end
