d7491afaa02d71aa9d68c6d32aa0e07ae062140e
Anton Argirov <anton.argirov@gmail.com>
2015-12-12 20:30:59 +0600

    Bump version to 0.0.6

--- init.rb
+++ init.rb
@@ -20,6 +20,9 @@ Rails.application.config.to_prepare do
   unless Group.include?(RedmineCustomWorkflows::GroupPatch)
     Group.send(:include, RedmineCustomWorkflows::GroupPatch)
   end
+  unless TimeEntry.include?(RedmineCustomWorkflows::TimeEntryPatch)
+    TimeEntry.send(:include, RedmineCustomWorkflows::TimeEntryPatch)
+  end
   unless WikiContent.include?(RedmineCustomWorkflows::WikiContentPatch)
     WikiContent.send(:include, RedmineCustomWorkflows::WikiContentPatch)
   end
@@ -38,7 +41,7 @@ Redmine::Plugin.register :redmine_custom_workflows do
   name 'Redmine Custom Workflow plugin'
   author 'Anton Argirov'
   description 'Allows to create custom workflows for issues, defined in the plain Ruby language'
-  version '0.1.5'
+  version '0.1.6'
   url 'http://www.redmine.org/plugins/custom-workflows'
 
   menu :admin_menu, :custom_workflows, {:controller => 'custom_workflows', :action => 'index'}, :caption => :label_custom_workflow_plural
