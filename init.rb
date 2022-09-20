9f874424bc2d47621271b1306f3f3628d203b718
Karel Pičman <karel.picman@kontron.com>
2022-09-20 13:03:25 +0200

    v2.0.6 devel started

--- init.rb
+++ init.rb
@@ -32,7 +32,7 @@ Redmine::Plugin.register :redmine_custom_workflows do
   end
   author 'Anton Argirov/Karel Pičman'
   description 'It allows to create custom workflows for objects, defined in a plain Ruby language'
-  version '2.0.5'
+  version '2.0.6 devel'
 
   requires_redmine version_or_higher: '4.1.0'
 
